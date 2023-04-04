981c67029545c2d12017ff709373e62fc86ef471
fernandogatto <fernandogatto17@gmail.com>
2023-04-04 17:52:50 -0300

    add: init ignite pomodoro project

---
+++ 2022/ignite-pomodoro/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from '@pages/Home'
+import History from '@pages/History'
+import DefaultLayout from './layouts/DefaultLayout'
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
