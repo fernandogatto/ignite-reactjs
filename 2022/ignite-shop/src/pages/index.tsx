8a3ac6f04d2a12f80d4175b521b705ee42b69ea6
fernandogatto <fernandogatto17@gmail.com>
2023-09-11 16:09:54 -0300

    add: slider products  at home

--- 2022/ignite-shop/src/pages/index.tsx
+++ 2022/ignite-shop/src/pages/index.tsx
@@ -1,5 +1,63 @@
+import Image from "next/image";
+
+import { useKeenSlider } from 'keen-slider/react';
+
+import { HomeContainer, ProductItem } from "@/styles/pages/home";
+
+import camiseta1 from "../assets/clothes/1.png";
+
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+    const [sliderRef] = useKeenSlider({
+        slides: {
+            perView: 3,
+            spacing: 48
+        }
+    });
+
     return (
-        <h1>Hello World</h1>
-    )
+        <HomeContainer ref={sliderRef} className="keen-slider">
+            <ProductItem className="keen-slider__slide">
+                <Image src={camiseta1} width={520} height={480} alt="1" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </ProductItem>
+            <ProductItem className="keen-slider__slide">
+                <Image src={camiseta1} width={520} height={480} alt="1" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </ProductItem>
+            <ProductItem className="keen-slider__slide">
+                <Image src={camiseta1} width={520} height={480} alt="1" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </ProductItem>
+            <ProductItem className="keen-slider__slide">
+                <Image src={camiseta1} width={520} height={480} alt="1" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </ProductItem>
+            <ProductItem className="keen-slider__slide">
+                <Image src={camiseta1} width={520} height={480} alt="1" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </ProductItem>
+        </HomeContainer>
+    );
 }
