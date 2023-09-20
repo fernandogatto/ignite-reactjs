f383dd409072668060ab46fd559b8645b81a330c
fernandogatto <fernandogatto17@gmail.com>
2023-09-20 20:07:43 -0300

    add: Users list

---
+++ 2021/dashgo/src/pages/users/index.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+import NextLink from "next/link";
+
+import { Box, Button, Checkbox, Flex, Heading, Icon, Link, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+export default function ListUsers({ users }) {
+    const [page, setPage] = useState(1);
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                {/* <Sidebar /> */}
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                        </Heading>
+
+                        <NextLink href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar novo
+                            </Button>
+                        </NextLink>
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
+                                <Th width="8"></Th>
+                            </Tr>
+                        </Thead>
+
+                        <Tbody>
+                            <Tr>
+                                <Td px={["4", "4", "6"]}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+
+                                <Td>
+                                    <Box>
+                                        <Link color="purple.400">
+                                            <Text fontWeight="bold">Fernando</Text>
+                                        </Link>
+                                        
+                                        <Text fontSize="sm" color="gray.300">fernandogatto17@gmail.com</Text>
+                                    </Box>
+                                </Td>
+
+                                {isWideVersion && <Td>20/09/2023</Td>}
+
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
