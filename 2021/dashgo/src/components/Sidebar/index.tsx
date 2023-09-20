2188ed63e5259b953731d138efeb3a842d7a5245
fernandogatto <fernandogatto17@gmail.com>
2023-09-20 17:04:27 -0300

    add: Sidebar

---
+++ 2021/dashgo/src/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+import { SidebarNav } from './SidebarNav';
+
+export default function Sidebar() {
+    return (
+        <Box>
+            <SidebarNav />
+        </Box>
+    )
+}
