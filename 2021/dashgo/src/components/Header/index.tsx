6ae22305d73befcab986ceeb9b158235894e35c9
fernandogatto <fernandogatto17@gmail.com>
2023-09-27 20:24:44 -0300

    V1

--- 2021/dashgo/src/components/Header/index.tsx
+++ 2021/dashgo/src/components/Header/index.tsx
@@ -1,30 +1,52 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 
-import Logo from './Logo'
-import Searchbox from './Searchbox'
-import { NotificationsNav } from './NotificationsNav'
+import { Logo } from './Logo';
+import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile'
+import { SearchBox } from './SearchBox';
 
-export default function Header() {
-    return (
-        <Flex
-            as="header"
-            w="100%"
-            maxWidth={1480}
-            h="20"
-            mx="auto"
-            mt="4"
-            px="6"
-            align="center"
+export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
         >
-            <Logo />
 
-            <Searchbox />
+        </IconButton>
+      ) }
 
-            <Flex align="center" ml="auto">
-                <NotificationsNav />
-                <Profile />
-            </Flex>
-        </Flex>
-    )
+      <Logo />
+
+      { isWideVersion && <SearchBox /> }
+    
+      <Flex align="center" ml="auto">
+        <NotificationsNav />
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+  );
 }
